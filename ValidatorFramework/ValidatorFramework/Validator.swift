6551d036dcaffec8bc589cb81a595710b7b70e35
Aasheesh Rai <aasheesh.rai@bell.ca>
2025-04-08 11:32:52 +0530

    added framework

---
+++ ValidatorFramework/ValidatorFramework/Validator.swift
@@ -0,0 +1,16 @@
+//
+//  Validator.swift
+//  ValidatorFramework
+//
+//  Created by Aasheesh Rai on 08/04/25.
+//
+
+import Foundation
+
+
+public struct Validator {
+    
+    public static func sayHello() {
+        print("Say hello to test framework!!")
+    }
+}
