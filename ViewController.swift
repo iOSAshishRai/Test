cd1a8da54f309045d85dd5fb0bea41b8c21edfa9
Aasheesh Rai <aasheesh.rai@bell.ca>
2025-04-08 10:39:09 +0530

    intial commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Aasheesh Rai on 07/04/25.
+//
+
+import UIKit
+import TestFramework
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Validator.sayHello()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
